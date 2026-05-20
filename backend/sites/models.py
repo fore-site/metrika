827575d9fa41774a0a189731913506ee80b48d6a
oluwaseun ogunsanu <ogunsanu17@gmail.com>
2026-05-20 04:29:31 +0100

    update deploy command

--- backend/sites/models.py
+++ backend/sites/models.py
@@ -1,7 +1,6 @@
 import uuid
 from django.conf import settings
 from django.db import models
-from typing import Callable, List, Tuple
 import zoneinfo
 
 from django.utils.deconstruct import deconstructible
