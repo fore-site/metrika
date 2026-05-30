de90ef3a585f2866f50b032783e7d7689b482997
oluwaseun ogunsanu <ogunsanu17@gmail.com>
2026-05-30 20:39:51 +0100

    implement demo views

---
+++ backend/analytics/demo_views.py
@@ -0,0 +1,69 @@
+from django.conf import settings
+from django.core.exceptions import ImproperlyConfigured
+from rest_framework.throttling import ScopedRateThrottle
+from rest_framework.permissions import AllowAny
+from sites.models import Site
+from .views import (
+    BaseStatsView,
+    SummaryView,
+    TimeseriesView,
+    TopPagesView,
+    TopReferrersView,
+    CountriesView,
+    DevicesView,
+    BrowsersView,
+    OSView,
+    TopRegionsView,
+    TopCitiesView,
+)
+
+class DemoBaseStatsView(BaseStatsView):
+    """
+    Overrides get_site to always return the demo site, bypassing ownership checks.
+    """
+    permission_classes = [AllowAny]
+    authentication_classes = []
+    throttle_classes = [ScopedRateThrottle]
+    throttle_scope = 'demo'
+
+    def get_site(self, site_id=None):
+        """Return the demo site, ignoring the provided site_id."""
+        site_id = getattr(settings, 'DEMO_SITE_ID', None)
+        if not site_id:
+            raise ImproperlyConfigured('DEMO_SITE_ID is not set in settings.')
+        try:
+            return Site.objects.get(id=site_id, is_active=True)
+        except Site.DoesNotExist:
+            return None
+
+
+# Now create public versions of each analytics view
+class DemoTimeseriesView(DemoBaseStatsView, TimeseriesView):
+    pass
+
+class DemoSummaryView(DemoBaseStatsView, SummaryView):
+    pass
+
+class DemoTopPagesView(DemoBaseStatsView, TopPagesView):
+    pass
+
+class DemoTopReferrersView(DemoBaseStatsView, TopReferrersView):
+    pass
+
+class DemoCountriesView(DemoBaseStatsView, CountriesView):
+    pass
+
+class DemoDevicesView(DemoBaseStatsView, DevicesView):
+    pass
+
+class DemoBrowsersView(DemoBaseStatsView, BrowsersView):
+    pass
+
+class DemoOSView(DemoBaseStatsView, OSView):
+    pass
+
+class DemoTopRegionsView(DemoBaseStatsView, TopRegionsView):
+    pass
+
+class DemoTopCitiesView(DemoBaseStatsView, TopCitiesView):
+    pass
--- backend/analytics/urls.py
+++ backend/analytics/urls.py
@@ -13,6 +13,20 @@ from .views import (
     trigger_aggregation,
 )
 
+from .demo_views import (
+    DemoTimeseriesView,
+    DemoSummaryView,
+    DemoTopPagesView,
+    DemoTopReferrersView,
+    DemoCountriesView,
+    DemoDevicesView,
+    DemoBrowsersView,
+    DemoOSView,
+    DemoTopRegionsView,
+    DemoTopCitiesView,
+)
+
+
 urlpatterns = [
     path('<int:site_id>/summary/', SummaryView.as_view(), name='summary'),
     path('<int:site_id>/timeseries/', TimeseriesView.as_view(), name='timeseries'),
@@ -24,5 +38,18 @@ urlpatterns = [
     path('<int:site_id>/os/', OSView.as_view(), name='os'),
     path('<int:site_id>/top-regions/', TopRegionsView.as_view(), name='top-regions'),
     path('<int:site_id>/top-cities/', TopCitiesView.as_view(), name='top-cities'),
+
+    # Public demo routes – ignore the site_id parameter
+    path('demo/<int:site_id>/summary/', DemoSummaryView.as_view(), name='demo-summary'),
+    path('demo/<int:site_id>/timeseries/', DemoTimeseriesView.as_view(), name='demo-timeseries'),
+    path('demo/<int:site_id>/top-pages/', DemoTopPagesView.as_view(), name='demo-top-pages'),
+    path('demo/<int:site_id>/top-referrers/', DemoTopReferrersView.as_view(), name='demo-top-referrers'),
+    path('demo/<int:site_id>/countries/', DemoCountriesView.as_view(), name='demo-countries'),
+    path('demo/<int:site_id>/devices/', DemoDevicesView.as_view(), name='demo-devices'),
+    path('demo/<int:site_id>/browsers/', DemoBrowsersView.as_view(), name='demo-browsers'),
+    path('demo/<int:site_id>/os/', DemoOSView.as_view(), name='demo-os'),
+    path('demo/<int:site_id>/top-regions/', DemoTopRegionsView.as_view(), name='demo-top-regions'),
+    path('demo/<int:site_id>/top-cities/', DemoTopCitiesView.as_view(), name='demo-top-cities'),
+
     path('internal/aggregate/', trigger_aggregation, name='trigger-aggregation'),
 ]
